d95446521d2ed34bdd5a7e952add87051ef5db57
santhanakrishnanstark <santhanakrishnanstark@gmail.com>
2022-06-04 10:27:33 +0530

    initial setup master

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <></>
+    <div className="App">
+      <h1>This is the Master Branch</h1>
+      <p>Go to other branches to see the codes</p>
+    </div>
   );
 }
 
